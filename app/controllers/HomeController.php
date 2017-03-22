5e5a524b0bf5aea8f69f0080a2865b510b4bbaf9
Yash Dev Lamba <yashdev18@gmail.com>
2017-03-23 04:21:55 +0530

    multiple tab session added and link in table fixed

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,11 +1,25 @@
 <?php 
 
 namespace Saved\Controllers;
+session_start();
+
+use Saved\Models\User;
 
 class HomeController extends BaseController{
 	public function get(){
-		//echo "this is get() in HomeController.php";
-		$this->render('index.html');
+		if(isset($_SESSION['flag'])){
+
+			$rows = User::show();
+			$users = User::showusers();
+			
+			if($_SESSION['login']['Username'] == 'admin'){
+				$this->render('admin.html',['rows' => $rows, 'login' => $_SESSION['login'], 'users' => $users]);
+			}else{
+				$this->render('profile.html',['rows' => $rows, 'login' => $_SESSION['login']]);
+			}
+		}else{
+			$this->render('index.html');
+		}
 	}
 }
 
